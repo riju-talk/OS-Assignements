6ff3992c42c732b8640f81587a73c3077b8334a0
riju-talk <talk.riju@gmail.com>
2023-08-23 10:30:30 +0530

    commit bich

---
+++ Assignment1/q1.2.1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int fib(int n){
+    if(n<2){
+        return n;
+    }
+    else{
+        return fib(n-1)+(n-2);
+    }
+}
+
+int fac(int n){
+    if(n<2){
+        return 1;
+    }
+    else{
+        return n*fac(n-1);
+    }
+}
+
+
+int main(){
+
+    int child=vfork();
+    
+    if(child<0){
+        printf("Fork Failed\n");
+    }
+    else if(child==0){
+        printf("%d\n",fac(4));
+    }
+    else{
+        wait(NULL);
+        printf("%d\n",fib(16));
+    }
+    return 0;
+}
--- Assignment1/q2.c
+++ Assignment1/q2.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 #include<time.h>
+#include<stdbool.h>
 
-int main(int argc,char *argv[]){
-    
+
+int main(int argc, char* argv[]){
+    while (1)
+    {   
+        
+    }
     return 0;
 }
