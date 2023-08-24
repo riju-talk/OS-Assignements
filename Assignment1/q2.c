f38a1f2080e14ff711668cde88750e7c7aeda065
riju-talk <talk.riju@gmail.com>
2023-08-24 20:54:39 +0530

    erasing

--- Assignment1/q2.c
+++ Assignment1/q2.c
@@ -6,11 +6,22 @@
 int main(){
     while (true)
     {
-        char *k;
-        printf("--+++======>>>>>");
-        scanf("%s",k);
+        char k[12];
+        char l[12];
+        char m[12];
+        printf(":::-----+++======>>>>> ");
+        scanf("%s %s %s",k,l,m);
         if(strcmp(k,"word")==0){
-            
+            printf("word\n");
+        }
+        else if(strcmp(k,"dir")==0){
+            printf("dir\n");
+        }
+        else if(strcmp(k,"date")==0){
+            printf("date\n");
+        }
+        else if(strcmp(k,"exit()")==0){
+            break;
         }
     }
     return 0;
