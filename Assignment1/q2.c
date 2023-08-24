7072f8333a7dc5819e7c745a9575426ce108b186
Rijusmit Biswas <112485983+riju-talk@users.noreply.github.com>
2023-08-24 12:28:35 +0530

    Update q2.c

--- Assignment1/q2.c
+++ Assignment1/q2.c
@@ -1,16 +1,40 @@
-#include<stdio.h>
-#include<time.h>
-#include<stdbool.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main(){
-    while (true)
-    {
-        char *k;
-        printf("--+++======>>>>>");
-        scanf("%s",k);
-        if(strcmp(k,"word")==0){
-            
+void splitString(char* input, char delimiter, char substrings[3][1000], int count) {
+    char* token = strtok(input, &delimiter);
+    int index = 0;
+
+    while (token != NULL && index < count) {
+        strcpy(substrings[index], token);
+        (index)++;
+        token = strtok(NULL, &delimiter);
+    }
+}
+
+int main() {
+    while (true) {
+        char set[3][1000];
+        char sentence[1000];
+        printf("--+++======>>>>> ");
+        fgets(sentence, sizeof(sentence), stdin);
+
+        // Remove the newline character from the input
+        sentence[strcspn(sentence, "\n")] = '\0';
+        
+        splitString(sentence,' ',set,3);
+        
+        if (strcmp(set[0], "word") == 0) {            
+            printf("Word\n");
+        } else if (strcmp(set[0], "dir") == 0) {
+            printf("dir\n");
+        } else if (strcmp(set[0], "date") == 0) {
+            printf("date\n");
+        } else if (strcmp(set[0], "exit()") == 0) {
+            break;
+        } else {
+            printf("Hey, this is your input: %s\n", sentence);
         }
     }
     return 0;
