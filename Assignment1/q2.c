5e69717578c91fe5276b56b96013add69c55ab6b
riju-talk <talk.riju@gmail.com>
2023-08-25 00:45:39 +0530

    time part done

--- Assignment1/q2.c
+++ Assignment1/q2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
+#include <sys/stat.h>
 
 void splitString(char* input, char delimiter, char substrings[3][1000], int count) {
     char* token = strtok(input, &delimiter);
@@ -30,7 +32,28 @@ int main() {
         } else if (strcmp(set[0], "dir") == 0) {
             printf("dir\n");
         } else if (strcmp(set[0], "date") == 0) {
-            printf("date\n");
+            struct stat info;
+            if(strcmp(set[1],"-d")==0){
+                stat(set[2],&info);
+                time_t mod_time = info.st_mtime;
+                struct tm date = *localtime(&mod_time);
+                printf("%d:%d:%d\n",date.tm_hour,date.tm_min,date.tm_sec);
+            }
+            else if(strcmp(set[1],"-R")==0){
+                stat(set[2],&info);
+                time_t mod_time = info.st_mtime;
+                struct tm date = *localtime(&mod_time);
+                char formattertime[200];
+                strftime(formattertime,200,"%a, %d %h %Y %T %z",&date);
+                printf("%s\n",formattertime);
+            }
+            else{
+                stat(set[1],&info);
+                time_t mod_time = info.st_mtime;
+                struct tm date = *localtime(&mod_time);
+                printf("%d/%d/%d %d:%d:%d\n",date.tm_mday,1+date.tm_mon,1900+date.tm_year,date.tm_hour,date.tm_min,date.tm_sec);
+            }
+ 
         } else if (strcmp(set[0], "exit()") == 0) {
             break;
         } else {
--- Assignment1/q2_terminal.c
+++ Assignment1/q2_terminal.c
@@ -1,31 +1,9 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<time.h>
+#include<stdbool.h>
+#include<string.h>
 
-int main() {
-    while (true) {
-        char set[5][1000];
-        char sentence[1000];
-        printf("--+++======>>>>> ");
-        fgets(sentence, sizeof(sentence), stdin);
-
-        // Remove the newline character from the input
-        sentence[strcspn(sentence, "\n")] = '\0';
-        
-        splitString(sentence,' ',set,5);
-        
-        if (strcmp(set[0], "word") == 0) {          
-            printf("Word\n");
-        } else if (strcmp(set[0], "dir") == 0) {
-            printf("dir\n");
-        } else if (strcmp(set[0], "date") == 0) {
-            printf("date\n");
-        } else if (strcmp(set[0], "exit()") == 0) {
-            break;
-        } else {
-            printf("%s\n", sentence);
-        }
-    }
+int main(int argc, char*argv[]){
+    
     return 0;
 }
